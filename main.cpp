ce0c3a791334d7f17879da99d8c238f8610efc51
gaurav <gaurav61989@gmail.com>
2019-11-03 09:12:39 +0530

    my first game commit in sfml

--- main.cpp
+++ main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
 
-int main(int argc, char **argv) {
-    std::cout << "Hello, world!" << std::endl;
-    return 0;
+#include "Game.h"
+using namespace std;
+using namespace sf;
+
+int main() {
+
+  // Code adapted from the SFML 2 "Window" example.
+
+  //cout << "Version " << myproject_VERSION_MAJOR << "." << myproject_VERSION_MINOR << endl;
+
+  RenderWindow App(VideoMode(800, 600), "myproject",Style::Titlebar|Style::Close);
+  Event Event; 
+  while (App.isOpen()) {
+    
+    while (App.pollEvent(Event)) {
+      switch(Event.type) 
+      {
+          case Event::Closed:
+              App.close();
+              break;
+          case Event::KeyPressed: 
+              if(Event.key.code == Keyboard::Escape)
+                  App.close();
+              break;
+              
+      
+    }
+    App.clear(Color(255,255,255,255));
+    App.display();
+  }
+}
 }
